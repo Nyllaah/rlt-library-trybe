db5c99376fffbd4ddd810796d91f0b2b01f2e6d6
Nyllaah <nylla.g@hotmail.com>
2023-09-13 11:13:08 -0300

    feat: req 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.tsx />', () => {
+  test('a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<App />, { route: '/404error' });
+    const notFoundMsg = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(notFoundMsg).toBeVisible();
+  });
+
+  test('a página mostra a imagem com o texto alternativo', () => {
+    renderWithRouter(<App />, { route: '/404error' });
+    const image = screen.getByAltText(/Clefairy pushing buttons randomly with text I have no idea what i'm doing/i);
+
+    expect(image).toBeVisible();
+  });
+});
