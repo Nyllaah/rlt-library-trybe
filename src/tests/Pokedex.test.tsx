1bbcf0a586f0d5131a8a90f602a57129850ff8f2
Nyllaah <nylla.g@hotmail.com>
2023-09-13 15:34:01 -0300

    feat: req 5 not passing

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import pokemonList from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+describe('este o componente <Pokedex.tsx />', () => {
+  test('a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByText('Encountered Pokémon');
+    expect(title).toBeVisible();
+  });
+
+  test('é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado:', () => {
+    renderWithRouter(<App />);
+
+    pokemonList.forEach(async (pokemon, index) => {
+      const currentPokemon = screen.getByTestId('pokemon-name');
+      const nextBtn = screen.getByTestId('next-pokemon');
+      expect(nextBtn).toBeVisible();
+      expect(currentPokemon).toHaveTextContent(pokemon.name);
+      await userEvent.click(nextBtn);
+      const nextPokemon = await screen.findByTestId('pokemon-name');
+      expect(nextPokemon).toHaveTextContent(pokemonList[index + 1].name);
+    });
+  });
+});
