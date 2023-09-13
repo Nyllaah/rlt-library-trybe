b0fbfe3d478db877679c66f995746bce67de46fe
Nyllaah <nylla.g@hotmail.com>
2023-09-13 15:43:06 -0300

    feat: req 5 test

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -11,17 +11,25 @@ describe('este o componente <Pokedex.tsx />', () => {
     expect(title).toBeVisible();
   });
 
-  test('é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado:', () => {
+  test('é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado:', async () => {
     renderWithRouter(<App />);
 
-    pokemonList.forEach(async (pokemon, index) => {
-      const currentPokemon = screen.getByTestId('pokemon-name');
-      const nextBtn = screen.getByTestId('next-pokemon');
-      expect(nextBtn).toBeVisible();
-      expect(currentPokemon).toHaveTextContent(pokemon.name);
-      await userEvent.click(nextBtn);
-      const nextPokemon = await screen.findByTestId('pokemon-name');
-      expect(nextPokemon).toHaveTextContent(pokemonList[index + 1].name);
-    });
+    // pokemonList.forEach(async (pokemon, index) => {
+    //   const currentPokemon = screen.getByTestId('pokemon-name');
+    //   const nextBtn = screen.getByTestId('next-pokemon');
+    //   expect(nextBtn).toBeVisible();
+    //   expect(currentPokemon).toHaveTextContent(pokemon.name);
+    //   await userEvent.click(nextBtn);
+    //   const nextPokemon = await screen.findByTestId('pokemon-name');
+    //   expect(nextPokemon).toHaveTextContent(pokemonList[index + 1].name);
+    // });
+
+    const currentPokemon = screen.getByTestId('pokemon-name');
+    const nextBtn = screen.getByTestId('next-pokemon');
+    expect(nextBtn).toBeVisible();
+    expect(currentPokemon).toHaveTextContent(pokemonList[0].name);
+    await userEvent.click(nextBtn);
+    const nextPokemon = await screen.findByTestId('pokemon-name');
+    expect(nextPokemon).toHaveTextContent(pokemonList[1].name);
   });
 });
